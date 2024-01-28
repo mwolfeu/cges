6e489d6f2a30efb959ab8b58fbf580edd18ec60c
mwolf <wolfm2@newschool.edu>
2024-01-28 13:34:32 +0100

    docs

---
+++ docs/_nuxt/events-news.e95199a9.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.7eb24b20.js";const s={};function t(n,r){return" Events News "}const _=e(s,[["render",t]]);export{_ as default};
---
+++ docs/_nuxt/resources.7d590866.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.7eb24b20.js";const r={};function c(s,o){return" Resources "}const _=e(r,[["render",c]]);export{_ as default};
