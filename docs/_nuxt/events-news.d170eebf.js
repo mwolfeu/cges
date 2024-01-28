d9e2ac01e778e13f23b6a91626b6d69036672eae
mwolf <wolfm2@newschool.edu>
2024-01-28 17:52:19 +0100

    getting pages working

---
+++ docs/_nuxt/events-news.d170eebf.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.76485939.js";const s={};function t(n,r){return" Events News "}const _=e(s,[["render",t]]);export{_ as default};
---
+++ docs/_nuxt/resources.b848b7f9.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.76485939.js";const r={};function c(s,o){return" Resources "}const _=e(r,[["render",c]]);export{_ as default};
