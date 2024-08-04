b0bbf0725a3603c7faa4c4e91b77cbb6a27fc443
mw <mw@gmail.com>
2024-08-04 19:52:48 +0200

    deploy

---
+++ docs/_nuxt/about.f0f2b5cb.js
@@ -0,0 +1,1 @@
+import{i as t}from"./ItemList.3fc9eef6.js";import{_ as e}from"./_plugin-vue_export-helper.c27b6911.js";import{n,q as r,o as s}from"./entry.c8de3f6a.js";import"./intersectionObserver.606f5621.js";const c={components:{il:t}};function p(a,i,m,_,f,l){const o=n("il");return s(),r(o,{contentKey:"About"})}const b=e(c,[["render",p]]);export{b as default};
